7f5fb37c0b2664e42a168a511f3a7d4e2f279ceb
a1357422 <a1357422@gmail.com>
2023-01-04 16:56:19 +0800

    2023/1/4 晚歸/外宿新增功能修復

---
+++ resources/views/leaves/form1.blade.php
@@ -0,0 +1,15 @@
+<div>
+    {!! Form::label('start','外宿日起：')!!}
+    {!! Form::date('start',null)!!}
+</div>
+<div>
+    {!! Form::label('end','外宿日訖：')!!}
+    {!! Form::date('end',null)!!}
+</div>
+<div>
+    {!! Form::label('reason','外宿原因：')!!}
+    {!! Form::text('reason',null)!!}
+</div>    
+<div>
+    {!! Form::submit($submitButtonText)!!}
+</div>
